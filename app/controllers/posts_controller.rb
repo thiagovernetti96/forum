5e957b286e6fded61d4194230fd4e2a374d6b632
THIAGO V <thaigovernetti@gmail.com>
2021-04-23 14:51:41 -0300

    views show e edit, mudanças no controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,50 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all.order("created_at DESC")
-         end
-    end
-   
-    def new
-     @post = Post.new
-    end 
-       
-    def edit
-    end
-    
-    def show
-     @post = Post.find(params[:id])
-    end
-    
-    def update
-     if @post.update(post_params)
-        redirect_to @post
-     else
-        render 'edit'
-     end
-    end
-    
-    def destroy
-     @post.destroy
-       redirect_to root_path
-    end
-    
-    def create
-     @post = Post.new(post_params)
-     if @post.save
-        redirect_to @post
-       else
-        render 'new'
-       end
-    end
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	
+	def index
+		@posts = Post.all.order("created_at DESC")
+	end
 
-    private def post_params
-        params.require(:post).permit(:title, :content)
-    end
-    
-    def find_post
-        @post = Post.find(params[:id])
-    end
+	def show
+	end
 
-   
+	def new
+		@post = Post.new
+	end 
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+end
